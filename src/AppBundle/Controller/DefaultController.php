d166ac9e03ca79af481f30e14770b897a82938e2
Daniel <daniloviciv@hotmail.com>
2017-09-12 00:07:25 +0200

    landing bitcoin price getting price from json api response

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Util\Apicall;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +20,42 @@ class DefaultController extends Controller
         return $this->render('AppBundle:default:index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/text/bitcoin-price", name="text-bitcoin-price")
+     */
+    public function textBitcoinAction(Request $request)
+    {
+        // replace this example code with whatever you need
+
+        $url = Apicall::BITCOIN_API_PRICE;
+
+        $apiCall = new Apicall();
+        $apiResponse = $apiCall->CallAPI('GET',$url);
+
+
+        $data = json_decode($apiResponse,true);
+
+        return $this->render('AppBundle:default:alfa.html.twig', [
+           'data'   => $data,
+        ]);
+    }
+
+    /**
+     * @Route("/json/bitcoin-price", name="json-bitcoin-price")
+     */
+    public function jsonBitcoinAction(Request $request)
+    {
+        // replace this example code with whatever you need
+
+        $url = Apicall::BITCOIN_API_PRICE;
+
+        $apiCall = new Apicall();
+        $apiResponse = $apiCall->CallAPI('GET',$url);
+
+        $JSONresponse = new JsonResponse($apiResponse);
+
+        return $JSONresponse;
     }
 }
