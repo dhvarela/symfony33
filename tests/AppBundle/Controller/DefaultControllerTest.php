bfc6e9217c7d55441c2829d42574dc55b40cf3f6
Daniel <daniloviciv@hotmail.com>
2017-09-12 00:42:23 +0200

    added 7 webTestCases to the application, chacking home and Bitcoin price landing

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -13,6 +13,26 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+
+        $this->assertGreaterThan(0,$crawler->filter('h1')->count());
+        $this->assertContains('Welcome to Symfony', $crawler->filter('.container h1')->text());
+
+        $this->assertGreaterThan(0,$crawler->filter('html:contains("Your application is now ready")')->count());
+
+        // go to bitcoin text price
+        $link = $crawler
+            ->filter('#left-menu a:contains("Bitcoin")') // find all links with the text "Greet"
+            //->eq(1) // select the second link in the list
+            ->link()
+        ;
+
+        $crawler = $client->click($link);
+
+        // checking for Bitcoin price h1
+        $this->assertContains('Bitcoin Price', $crawler->filter('.container h1')->text());
+
+        // checking for 3 currency columns
+        $this->assertEquals(3,$crawler->filter('.bitcoin-table h3')->count());
+
     }
 }
